12688e0ddf8767f22b5df13e1507241a6ea08c08
Alex Rozgo <alex.rozgo@gmail.com>
2021-05-02 04:38:02 -0600

    WIP multiple currencies support

---
+++ primitives/src/assets.rs
@@ -0,0 +1,14 @@
+use codec::{Decode, Encode};
+#[cfg(feature = "std")]
+use serde::{Deserialize, Serialize};
+use sp_core::{RuntimeDebug, H160};
+
+#[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, Ord, PartialOrd, RuntimeDebug)]
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+pub enum AssetId {
+    SUGAR,
+    DOT, // TODO: Enabled in Parachain upgrade
+    CHAINSAFE(H160),
+    TOKEN(H160),
+   // PARACHAIN(para_id, network, palletInstance, assetID),
+}
