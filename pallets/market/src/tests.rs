7ba72053d0e819f551c240b0dfb3b58d0459e729
Alex Rozgo <alex.rozgo@gmail.com>
2022-01-13 01:18:34 -0600

    initial market tests

--- pallets/market/src/tests.rs
+++ pallets/market/src/tests.rs
@@ -1,12 +1,12 @@
-use crate::mock::*;
+use crate::{mock::*, AssetRate, MarketRate, RateAmount, RateTarget};
 use frame_support::assert_ok;
 
-// fn last_event() -> Event {
-//     frame_system::Pallet::<Test>::events()
-//         .pop()
-//         .expect("Event expected")
-//         .event
-// }
+fn last_event() -> Event {
+    frame_system::Pallet::<Test>::events()
+        .pop()
+        .expect("Event expected")
+        .event
+}
 
 pub fn before_market() {
     run_to_block(10);
@@ -47,5 +47,73 @@ pub fn before_market() {
 fn before_market_works() {
     new_test_ext().execute_with(|| {
         before_market();
+    })
+}
+
+#[test]
+fn create_market_works() {
+    new_test_ext().execute_with(|| {
+        before_market();
+
+        assert_ok!(Market::do_create_market(&1, 1000));
+
+        assert_eq!(
+            last_event(),
+            Event::Market(crate::Event::Created {
+                market_id: 1000,
+                who: 1,
+            }),
+        );
+    })
+}
+
+#[test]
+fn create_market_rate_works() {
+    new_test_ext().execute_with(|| {
+        before_market();
+
+        assert_ok!(Market::do_create_market(&1, 1000));
+
+        assert_eq!(
+            last_event(),
+            Event::Market(crate::Event::Created {
+                market_id: 1000,
+                who: 1,
+            }),
+        );
+
+        let market_rate = MarketRate {
+            goods: vec![
+                AssetRate {
+                    class_id: 2000,
+                    asset_id: 1,
+                    amount: RateAmount::Credit(5),
+                    target: RateTarget::Buyer,
+                },
+                AssetRate {
+                    class_id: 2000,
+                    asset_id: 2,
+                    amount: RateAmount::Credit(5),
+                    target: RateTarget::Buyer,
+                },
+            ],
+            price: vec![
+                AssetRate {
+                    class_id: 3000,
+                    asset_id: 1,
+                    amount: RateAmount::Debit(1),
+                    target: RateTarget::Buyer,
+                },
+                AssetRate {
+                    class_id: 3000,
+                    asset_id: 2,
+                    amount: RateAmount::Debit(1),
+                    target: RateTarget::Buyer,
+                },
+            ],
+            metadata: vec![],
+        };
+
+        assert_ok!(Market::do_create_market_rate(&1, 1000, 1, &market_rate));
     })
 }
