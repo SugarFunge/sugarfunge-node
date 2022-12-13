27d56fdd27613be33cbbf56955a4b529f8bc80bb
carloslopezandara <lopezandara.carlos@gmail.com>
2022-12-13 01:43:36 +0100

    Reversed some test changes

--- pallets/exgine/src/tests.rs
+++ pallets/exgine/src/tests.rs
@@ -4,14 +4,11 @@ use frame_support::{assert_noop, assert_ok};
 #[test]
 fn it_works_for_default_value() {
     new_test_ext().execute_with(|| {
-        // Go past genesis block so events get deposited
-		System::set_block_number(1);
         // Dispatch a signed extrinsic.
         assert_ok!(Exgine::do_something(RuntimeOrigin::signed(1), 42));
         // Read pallet storage and assert an expected result.
         assert_eq!(Exgine::something(), Some(42));
         // Assert that the correct event was deposited
-		System::assert_last_event(Event::SomethingStored { something: 42, who: 1 }.into());
     });
 }
 
