bd6b203e9f598169faec6005923c35de65634f97
MauricinhoMorales <mauricio1999morales@gmail.com>
2023-11-14 22:30:38 -0500

    Updated the mock files for all pallets

--- pallets/asset/src/mock.rs
+++ pallets/asset/src/mock.rs
@@ -1,7 +1,9 @@
 use super::*;
 use crate as sugarfunge_asset;
-use frame_support::{parameter_types, traits::Everything};
-use frame_system as system;
+use frame_support::{
+    parameter_types,
+    traits::{ConstU128, ConstU16, ConstU32, ConstU64, Everything},
+};
 use sp_core::H256;
 use sp_runtime::{
     traits::{BlakeTwo256, IdentityLookup},
@@ -15,21 +17,40 @@ pub const DOLLARS: Balance = 100 * CENTS;
 
 parameter_types! {
     pub const CreateAssetClassDeposit: Balance = 500 * MILLICENTS;
-    pub const CreateCurrencyClassDeposit: Balance = 500 * MILLICENTS;
     pub const CreateBagDeposit: Balance = 1;
 }
 
-parameter_types! {
-    pub const ExistentialDeposit: u128 = 500;
-    pub const MaxClassMetadata: u32 = 1;
-    pub const MaxAssetMetadata: u32 = 1;
+impl frame_system::Config for Test {
+    type BaseCallFilter = Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type RuntimeCall = RuntimeCall;
+    type Nonce = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Block = frame_system::mocking::MockBlock<Test>;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = ConstU64<250>;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ConstU16<42>;
+    type OnSetCode = ();
+    type MaxConsumers = ConstU32<16>;
 }
 
 impl pallet_balances::Config for Test {
     type Balance = Balance;
     type RuntimeEvent = RuntimeEvent;
     type DustRemoval = ();
-    type ExistentialDeposit = ExistentialDeposit;
+    type ExistentialDeposit = ConstU128<500>;
     type AccountStore = System;
     type WeightInfo = pallet_balances::weights::SubstrateWeight<Test>;
     type MaxLocks = ();
@@ -47,13 +68,10 @@ impl sugarfunge_asset::Config for Test {
     type Currency = Balances;
     type AssetId = u64;
     type ClassId = u64;
-    type MaxClassMetadata = MaxClassMetadata;
-    type MaxAssetMetadata = MaxAssetMetadata;
+    type MaxClassMetadata = ConstU32<1>;
+    type MaxAssetMetadata = ConstU32<1>;
 }
 
-type Block = frame_system::mocking::MockBlock<Test>;
-
-// Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
     pub enum Test
     {
@@ -62,37 +80,6 @@ frame_support::construct_runtime!(
         Asset: sugarfunge_asset,
     }
 );
-
-parameter_types! {
-    pub const BlockHashCount: u64 = 250;
-    pub const SS58Prefix: u8 = 42;
-}
-
-impl system::Config for Test {
-    type BaseCallFilter = Everything;
-    type BlockWeights = ();
-    type BlockLength = ();
-    type DbWeight = ();
-    type RuntimeOrigin = RuntimeOrigin;
-    type RuntimeCall = RuntimeCall;
-    type Nonce = u64;
-    type Hash = H256;
-    type Hashing = BlakeTwo256;
-    type AccountId = u64;
-    type Lookup = IdentityLookup<Self::AccountId>;
-    type Block = Block;
-    type RuntimeEvent = RuntimeEvent;
-    type BlockHashCount = BlockHashCount;
-    type Version = ();
-    type PalletInfo = PalletInfo;
-    type AccountData = pallet_balances::AccountData<Balance>;
-    type OnNewAccount = ();
-    type OnKilledAccount = ();
-    type SystemWeightInfo = ();
-    type SS58Prefix = SS58Prefix;
-    type OnSetCode = ();
-    type MaxConsumers = frame_support::traits::ConstU32<16>;
-}
 
 pub fn new_test_ext() -> sp_io::TestExternalities {
     let mut t = frame_system::GenesisConfig::<Test>::default()
--- pallets/market/src/mock.rs
+++ pallets/market/src/mock.rs
@@ -1,14 +1,14 @@
+use super::*;
 use crate as sugarfunge_market;
 use frame_support::{
     parameter_types,
-    traits::{Everything, OnFinalize, OnInitialize},
+    traits::{ConstU128, ConstU16, ConstU32, ConstU64, Everything, OnFinalize, OnInitialize},
     PalletId,
 };
-use frame_system as system;
 use sp_core::H256;
 use sp_runtime::{
-    testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
+    BuildStorage,
 };
 use sugarfunge_primitives::Balance;
 
@@ -27,32 +27,6 @@ parameter_types! {
     pub const MaxAssetMetadata: u32 = 1;
 }
 
-impl pallet_balances::Config for Test {
-    type Balance = Balance;
-    type RuntimeEvent = RuntimeEvent;
-    type DustRemoval = ();
-    type ExistentialDeposit = ExistentialDeposit;
-    type AccountStore = System;
-    type WeightInfo = pallet_balances::weights::SubstrateWeight<Test>;
-    type MaxLocks = ();
-    type MaxReserves = ();
-    type ReserveIdentifier = [u8; 8];
-	type HoldIdentifier = ();
-	type FreezeIdentifier = ();
-	type MaxHolds = ();
-	type MaxFreezes = ();
-}
-
-impl sugarfunge_asset::Config for Test {
-    type RuntimeEvent = RuntimeEvent;
-    type CreateAssetClassDeposit = CreateAssetClassDeposit;
-    type Currency = Balances;
-    type AssetId = u64;
-    type ClassId = u64;
-    type MaxClassMetadata = MaxClassMetadata;
-    type MaxAssetMetadata = MaxAssetMetadata;
-}
-
 parameter_types! {
     pub const BundleModuleId: PalletId = PalletId(*b"sug/bndl");
     pub const MarketModuleId: PalletId = PalletId(*b"sug/mrkt");
@@ -62,6 +36,58 @@ parameter_types! {
     pub const MaxAssets: u32 = 20;
     pub const MaxRates: u32 = 20;
     pub const MaxMetadata: u32 = 256;
+}
+
+impl frame_system::Config for Test {
+    type BaseCallFilter = Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type RuntimeCall = RuntimeCall;
+    type Nonce = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Block = frame_system::mocking::MockBlock<Test>;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = ConstU64<250>;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<Balance>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ConstU16<42>;
+    type OnSetCode = ();
+    type MaxConsumers = ConstU32<16>;
+}
+
+impl pallet_balances::Config for Test {
+    type Balance = Balance;
+    type RuntimeEvent = RuntimeEvent;
+    type DustRemoval = ();
+    type ExistentialDeposit = ConstU128<500>;
+    type AccountStore = System;
+    type WeightInfo = pallet_balances::weights::SubstrateWeight<Test>;
+    type MaxLocks = ();
+    type MaxReserves = ();
+    type ReserveIdentifier = [u8; 8];
+    type FreezeIdentifier = ();
+    type MaxHolds = ();
+    type MaxFreezes = ();
+    type RuntimeHoldReason = ();
+}
+
+impl sugarfunge_asset::Config for Test {
+    type RuntimeEvent = RuntimeEvent;
+    type CreateAssetClassDeposit = CreateAssetClassDeposit;
+    type Currency = Balances;
+    type AssetId = u64;
+    type ClassId = u64;
+    type MaxClassMetadata = ConstU32<1>;
+    type MaxAssetMetadata = ConstU32<1>;
 }
 
 impl sugarfunge_bundle::Config for Test {
@@ -80,59 +106,20 @@ impl sugarfunge_market::Config for Test {
     type MaxMetadata = MaxMetadata;
 }
 
-type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
-type Block = frame_system::mocking::MockBlock<Test>;
-
-// Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-    pub enum Test where
-        Block = Block,
-        NodeBlock = Block,
-        UncheckedExtrinsic = UncheckedExtrinsic,
+    pub enum Test
     {
-        System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
-        Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-        Asset: sugarfunge_asset::{Pallet, Call, Storage, Event<T>},
-        Bundle: sugarfunge_bundle::{Pallet, Call, Storage, Event<T>},
-        Market: sugarfunge_market::{Pallet, Call, Storage, Event<T>},
+        System: frame_system,
+        Balances: pallet_balances,
+        Asset: sugarfunge_asset,
+        Bundle: sugarfunge_bundle,
+        Market: sugarfunge_market,
     }
 );
 
-parameter_types! {
-    pub const BlockHashCount: u64 = 250;
-    pub const SS58Prefix: u8 = 42;
-}
-
-impl system::Config for Test {
-    type BaseCallFilter = Everything;
-    type BlockWeights = ();
-    type BlockLength = ();
-    type DbWeight = ();
-    type RuntimeOrigin = RuntimeOrigin;
-    type RuntimeCall = RuntimeCall;
-    type Index = u64;
-    type BlockNumber = u64;
-    type Hash = H256;
-    type Hashing = BlakeTwo256;
-    type AccountId = u64;
-    type Lookup = IdentityLookup<Self::AccountId>;
-    type Header = Header;
-    type RuntimeEvent = RuntimeEvent;
-    type BlockHashCount = BlockHashCount;
-    type Version = ();
-    type PalletInfo = PalletInfo;
-    type AccountData = pallet_balances::AccountData<Balance>;
-    type OnNewAccount = ();
-    type OnKilledAccount = ();
-    type SystemWeightInfo = ();
-    type SS58Prefix = SS58Prefix;
-    type OnSetCode = ();
-    type MaxConsumers = frame_support::traits::ConstU32<16>;
-}
-
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    let mut t = frame_system::GenesisConfig::default()
-        .build_storage::<Test>()
+    let mut t = frame_system::GenesisConfig::<Test>::default()
+        .build_storage()
         .unwrap();
     pallet_balances::GenesisConfig::<Test> {
         balances: vec![(1, 1000000 * DOLLARS), (2, 1000000 * DOLLARS)],
